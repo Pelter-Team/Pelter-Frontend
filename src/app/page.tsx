5670d52cd28d6eb885c54898fc9447514fff0c65
Mark169Z <90457798+Mark169Z@users.noreply.github.com>
2024-10-28 22:29:18 +0700

    static image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 import Link from "next/link"
-import { CldImage } from "next-cloudinary"
+import Image from "next/image"
+import Pelter1 from "./public/Pelter_1.jpeg"
+import Pelter2 from "./public/Pelter_2.png"
+import Pelter3 from "./public/Pelter_3.jpeg"
 
 export default function Home() {
   return (
@@ -9,13 +12,12 @@ export default function Home() {
         <div className="absolute italic top-[15%] z-20 text-9xl font-bold text-white">
           Pelter
         </div>
-        <CldImage
-          src="Pelter_2"
-          alt="Description of my image"
-          width="960"
-          height="600"
+        <Image
+          src={Pelter2}
+          width="1920"
+          height="1080"
           className="w-10/12 h-auto z-10"
-          sizes="100vw"
+          alt="LandingPicture"
         />
       </div>
 
@@ -28,11 +30,11 @@ export default function Home() {
             href="/"
             className="relative w-2/5 flex items-center justify-center duration-200 ease-in-out hover:scale-105"
           >
-            <CldImage
-              src="Pelter_1"
+            <Image
+              src={Pelter1}
               alt="Description of my image"
               width="960"
-              height="600"
+              height="500"
               sizes="100vw"
               className="rounded-md"
             />
@@ -46,8 +48,8 @@ export default function Home() {
             href="/"
             className="relative w-2/5 flex items-center justify-center duration-200 ease-in-out hover:scale-105"
           >
-            <CldImage
-              src="Pelter_3"
+            <Image
+              src={Pelter3}
               alt="Description of my image"
               width="500"
               height="500"
