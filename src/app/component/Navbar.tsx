0c5bc864f61e98701f0d0d7c9bb6f065008e0eaa
Mark169Z <ksd.markmax@hotmail.com>
2024-11-01 20:23:01 +0700

    Style Changed

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -1,53 +1,42 @@
 import Link from "next/link"
-import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons"
+import {
+  SearchOutlined,
+  ShoppingCartOutlined,
+  MenuOutlined,
+} from "@ant-design/icons"
 
-export default function Navbar() {
-  const menu = [
-    { name: "Pets", link: "/" },
-    { name: "Mock1", link: "/" },
-    { name: "Mock2", link: "/" },
-  ]
+export default function Navbar({ white }: { white?: boolean }) {
   return (
-    <div className="z-30 flex absolute items-center justify-between px-4 md:px-8 py-2 lg:px-12 lg:py-4 h-[4.5rem] w-full bg-transparent text-white">
-      <div className="flex flex-row">
-        <Link href="/" className="text-2xl font-bold mr-20 text-white">
+    <div
+      className={`z-30 flex fixed items-center justify-between px-4 md:px-8 py-2 lg:px-12 lg:py-4 h-[4.5rem] w-full ${
+        white ? "bg-white text-primary" : "bg-transparent text-white"
+      }`}
+    >
+      <div className="flex flex-row gap-6 items-center">
+        <MenuOutlined className="text-2xl hover:text-black cursor-pointer" />
+        <Link href="/" className="text-4xl font-bold mr-20 font-lobster">
           Pelter
         </Link>
-
-        <div className="flex gap-11 items-center justify-center">
-          {menu.map((item, index) => (
-            <Link
-              href={item.link}
-              key={index}
-              className="text-lg font-semibold"
-            >
-              <div className="group relative inline-block">
-                {item.name}
-                <div className="hidden group-hover:block absolute border-t-2 border-white -bottom-1 left-0 w-full"></div>
-              </div>
-            </Link>
-          ))}
-        </div>
       </div>
 
       <div className="flex flex-row gap-6 justify-center items-center">
         <Link href="/">
-          <HeartOutlined className="text-3xl hover:text-black" />
+          <SearchOutlined className="text-3xl hover:text-black" />
         </Link>
         <Link href="/">
           <ShoppingCartOutlined className="text-3xl hover:text-black" />
         </Link>
         <Link
           href="/"
-          className="flex py-2 px-4 border-2 border-yellow-800 bg-yellow-800 text-white justify-center items-center rounded-md hover:bg-yellow-600"
+          className="flex py-2 px-4 border-2 border-yellow-800 bg-yellow-800 text-white justify-center items-center rounded-md hover:bg-yellow-700 hover:border-yellow-700"
         >
           Sign Up
         </Link>
         <Link
           href="/"
-          className="flex py-2 px-4 border-2 justify-center items-center rounded-md hover:bg-white text-white hover:text-black"
+          className={`flex py-2 px-4 border-2 justify-center items-center rounded-md ${white ? "bg-secondary border-secondary hover:bg-#E9C9C1 hover:bg-opacity-80 text-black" : "border-white hover:bg-white text-white hover:text-black"}`}
         >
-          Sign In
+          Log In
         </Link>
       </div>
     </div>
