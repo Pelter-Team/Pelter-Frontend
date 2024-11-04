50e4e4387cd168056993053c6164e18fac84180b
Pornchanok Pinnuwat <pearp.pinnuwat@gmail.com>
2024-11-04 17:00:44 +0700

    Update : Navbar colour

--- src/app/component/NavbarLogin.tsx
+++ src/app/component/NavbarLogin.tsx
@@ -10,25 +10,28 @@ export default function NavbarLogin({ white }: { white?: boolean }) {
   return (
     <div
       className={`z-30 flex fixed items-center justify-between px-4 md:px-8 py-2 lg:px-12 lg:py-4 h-[4.5rem] w-full backdrop-blur-sm ${
-        white ? "bg-[#FFFAF5] text-primary" : "bg-white/80 text-[#C5705D]"
+        white ? "bg-[#FFFAF5] text-primary" : "bg-FFFAF5 text-[#C5705D]"
       }`}
     >
       <div className="flex flex-row gap-6 items-center">
         <MenuOutlined className="text-2xl hover:text-black cursor-pointer" />
-        <Link href="/" className="text-4xl font-bold mr-20 font-lobster">
+        <Link
+          href="/"
+          className="text-[45.33px] font-normal mr-20 font-lobster text-[#CD705D]"
+        >
           Pelter
         </Link>
       </div>
 
       <div className="flex flex-row gap-6 justify-center items-center">
         <Link href="/">
-          <SearchOutlined className="text-3xl hover:text-black" />
+          <SearchOutlined className="text-2xl hover:text-black" />
         </Link>
         <Link href="/">
-          <ShoppingCartOutlined className="text-3xl hover:text-black" />
+          <ShoppingCartOutlined className="text-2xl hover:text-black" />
         </Link>
         <Link href="/">
-          <PictureOutlined className="text-3xl hover:text-black" />
+          <PictureOutlined className="text-2xl hover:text-black" />
         </Link>
       </div>
     </div>
