d562c194f394c4dd1d7968df9f48c4a34939d118
Pornchanok Pinnuwat <pearp.pinnuwat@gmail.com>
2024-11-04 15:35:28 +0700

    Add : Navbar for after login

---
+++ src/app/component/NavbarLogin.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link"
+import {
+  SearchOutlined,
+  ShoppingCartOutlined,
+  MenuOutlined,
+  PictureOutlined,
+} from "@ant-design/icons"
+
+export default function NavbarLogin({ white }: { white?: boolean }) {
+  return (
+    <div
+      className={`z-30 flex fixed items-center justify-between px-4 md:px-8 py-2 lg:px-12 lg:py-4 h-[4.5rem] w-full ${
+        white ? "bg-[#FFFAF5] text-primary" : "bg-transparent text-[#C5705D]"
+      }`}
+    >
+      <div className="flex flex-row gap-6 items-center">
+        <MenuOutlined className="text-2xl hover:text-black cursor-pointer" />
+        <Link href="/" className="text-4xl font-bold mr-20 font-lobster">
+          Pelter
+        </Link>
+      </div>
+
+      <div className="flex flex-row gap-6 justify-center items-center">
+        <Link href="/">
+          <SearchOutlined className="text-3xl hover:text-black" />
+        </Link>
+        <Link href="/">
+          <ShoppingCartOutlined className="text-3xl hover:text-black" />
+        </Link>
+        <Link href="/">
+          <PictureOutlined className="text-3xl hover:text-black" />
+        </Link>
+      </div>
+    </div>
+  )
+}
