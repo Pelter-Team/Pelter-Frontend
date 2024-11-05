dc924c6004ee5dcb9b6633b1450bcc6a3b437f64
tgrziminiar <mixzazazaza1212@gmail.com>
2024-11-05 15:07:03 +0700

    chore: install rechartjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#C5705D",
+          // hover: "#AED8FB",
+          // active: "#96CFFA",
+        },
+        accent: {
+          DEFAULT: "#873800",
+        },
+        // D0B8A8
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
