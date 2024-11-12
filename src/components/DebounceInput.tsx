327a1737a6d9f5866f8ffcc51811c52b5a83a98e
tgrziminiar <mixzazazaza1212@gmail.com>
2024-11-12 16:14:22 +0700

    refactor: debounce input

--- src/components/DebounceInput.tsx
+++ src/components/DebounceInput.tsx
@@ -1,33 +1,19 @@
-import { forwardRef } from "react"
-import {
-  DebounceInputProps,
-  DebounceInput as ReactDebounceInput,
-} from "react-debounce-input"
+import { DebounceInput as ReactDebounceInput } from "react-debounce-input"
 
-interface CustomDebounceInputProps
-  extends Omit<
-    DebounceInputProps<
-      HTMLInputElement,
-      React.InputHTMLAttributes<HTMLInputElement>
-    >,
-    "onChange" | "size"
-  > {
+export const DebounceInput = ({
+  onChange,
+  className,
+}: {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
   className?: string
-}
-
-export const DebounceInput = forwardRef<
-  HTMLInputElement,
-  CustomDebounceInputProps
->(({ onChange, className, ...props }) => {
+}) => {
   return (
     <ReactDebounceInput
       minLength={2}
       className={className}
       onChange={onChange}
-      {...props}
     />
   )
-})
+}
 
 DebounceInput.displayName = "DebounceInput"
