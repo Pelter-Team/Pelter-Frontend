d9df8edc588749d8e41e66e34f8d1a8ee762f5d4
tgrziminiar <mixzazazaza1212@gmail.com>
2024-11-12 16:16:37 +0700

    refactor: refact more

--- src/components/DebounceInput.tsx
+++ src/components/DebounceInput.tsx
@@ -3,15 +3,24 @@ import { DebounceInput as ReactDebounceInput } from "react-debounce-input"
 export const DebounceInput = ({
   onChange,
   className,
+  debounceTimeout,
+  value,
+  placeholder,
 }: {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
   className?: string
+  debounceTimeout: number
+  value: any
+  placeholder?: string
 }) => {
   return (
     <ReactDebounceInput
+      value={value}
       minLength={2}
       className={className}
       onChange={onChange}
+      debounceTimeout={debounceTimeout}
+      placeholder={placeholder}
     />
   )
 }
