26701346e9d3d4bcf3661d15216c5792fef14a84
Mark169Z <90457798+Mark169Z@users.noreply.github.com>
2024-11-20 21:56:42 +0700

    saved

---
+++ src/app/productManage/page.tsx
@@ -0,0 +1,16 @@
+export default function Manage() {
+  
+    return (
+      <>
+       <div className="min-h-screen h-fit w-screen bg-mainBG px-16 flex flex-row">
+        <div className="flex w-1/3 p-8">
+
+        </div>
+        <div className="flex w-2/3 p-8">
+
+        </div>
+       </div>
+      </>
+    )
+  }
+  
